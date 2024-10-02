b456b4ef615a0cede7b83f6ab408a56117ea41d0
tabodino <tabodino@gmail.com>
2024-10-02 15:59:00 +0200

    Add SplashScreen Activity

---
+++ app/src/main/java/fr/wesy/sevenminutesworkout/presentation/ui/SplashScreenActivity.kt
@@ -0,0 +1,35 @@
+package fr.wesy.sevenminutesworkout.presentation.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AlphaAnimation
+import androidx.appcompat.app.AppCompatActivity
+import fr.wesy.sevenminutesworkout.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val logo = findViewById<ImageView>(R.id.splashLogo)
+        val fadeIn = AlphaAnimation(0.0f, 1.0f).apply {
+            duration = 2000
+            fillAfter = true
+        }
+       // logo.startAnimation(fadeIn)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
